cbeacbf2c9f97c9d88fcffd7b221237b2a6c6994
maicol-afk <crbobaldo427@gmail.com>
2024-06-21 23:09:54 +0200

    primera version

---
+++ app.js
@@ -0,0 +1,10 @@
+const coberturas = document.getElementsByClassName('cobertura')
+
+
+function mostrarClic(e) {
+    console.log(e.target.innerText);
+
+}
+for (const cobertura of coberturas) {
+    cobertura.addEventListener('click', mostrarClic);
+} 
